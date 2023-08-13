8062a13c5395abde08c551f95d24d0b25db21c3a
jipsonkj <141637734+jipsonkj@users.noreply.github.com>
2023-08-13 14:56:58 +0530

    Add files via upload

---
+++ bin/Dart Programs/leap_year or not.dart
@@ -0,0 +1,21 @@
+void main(){
+  int year=2012;
+
+  if(isLeapYear(year)){
+    print('$year is a leap year');
+  } else{
+    print('$year is not a leap year');
+  }
+}
+
+bool isLeapYear(int year){
+  if(year%4!=0){
+    return false;
+  } else if(year%100!=0){
+    return true;
+  } else if(year%400!=0){
+    return false;
+  } else{
+    return true;
+  }
+}
