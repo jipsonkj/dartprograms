0c01113a60134664d03fc9c11bcc4f94e15d32b8
jipsonkj <141637734+jipsonkj@users.noreply.github.com>
2023-08-19 16:29:41 +0530

    Add files via upload

--- bin/Dart Programs/exception_handling.dart
+++ bin/Dart Programs/exception_handling.dart
@@ -1,34 +1,10 @@
-abstract class Shape {
-  double calculateArea();
-}
-
-class Circle implements Shape {
-  double radius;
-
-  Circle(this.radius);
-
-  @override
-  double calculateArea() {
-    return 3.14159 * radius * radius;
-  }
-}
-
-class Rectangle implements Shape {
-  double width;
-  double height;
-
-  Rectangle(this.width, this.height);
-
-  @override
-  double calculateArea() {
-    return width * height;
-  }
-}
-
 void main() {
-  final circle = Circle(5);
-  final rectangle = Rectangle(4, 6);
+  List<int> numbers = [1, 2, 3, 4, 5];
+  int sum = 0;
 
-  print('Circle area: ${circle.calculateArea()}');
-  print('Rectangle area: ${rectangle.calculateArea()}');
+  for (int number in numbers) {
+    sum += number;
+  }
+
+  print('Sum of numbers: $sum');
 }
