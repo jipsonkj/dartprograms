7ba2aa72e6c48ed0f03e7d14c436d5067238c0e8
jipsonkj <141637734+jipsonkj@users.noreply.github.com>
2023-09-07 21:25:54 +0530

    Add files via upload

---
+++ bin/Dart Programs/listview_separate.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(builder1());
+}
+class builder1 extends StatelessWidget {
+
+
+  final List entries = ['A', 'B', 'C'];
+  final List colorCodes = [600, 500, 100];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) => const Divider(),
+          )
+      ),
+    );
+  }
+}
